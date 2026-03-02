47ee4298039101be15f67a7ec6d4c2e3ea1b4dce
Aybüke <119972157+aybukeKrcvs@users.noreply.github.com>
2026-03-02 22:14:13 +0300

    Initial final commit

---
+++ Assets/Scripts/FloatingMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class FloatingMovement : MonoBehaviour
+{
+    public float spped = 3f;
+    public float height = 0.4f;
+
+    private Vector3 startPos;
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
+    private void Update()
+    {
+        float newY = startPos.y + Mathf.Sin(Time.time * spped) * height;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+}
